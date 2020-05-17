94562ac6bb90af7e3dfb412387ba327ef9ccc729
fid3101 <hafiduddin_31@apps.ipb.ac.id>
2020-05-18 01:20:03 +0700

    commit ngubahkelola jadi cardview.xml

--- app/src/main/java/com/example/elis/kelola.java
+++ app/src/main/java/com/example/elis/kelola.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
+
+import io.grpc.NameResolver;
 
 
 /**
@@ -17,31 +20,25 @@ import android.view.ViewGroup;
 public class kelola extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_JENIS = "jenis";
+    private static final String ARG_RUANGAN = "ruangan";
+    private static final String ARG_MSWITCH = "mswith";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String jenis;
+    private String ruangan;
+    private Switch mswitch;
 
     public kelola() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment kelola.
-     */
     // TODO: Rename and change types and number of parameters
-    public static kelola newInstance(String param1, String param2) {
+    public static kelola newInstance(String jenis, String ruangan, Switch mswitch) {
         kelola fragment = new kelola();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_JENIS, jenis);
+        args.putString(ARG_RUANGAN, ruangan);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +47,8 @@ public class kelola extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            jenis = getArguments().getString(ARG_JENIS);
+            ruangan = getArguments().getString(ARG_RUANGAN);
         }
     }
 
