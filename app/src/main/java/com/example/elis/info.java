84dfee4fbfbc9b7f1c7572e1aa955842c190755b
fid3101 <hafiduddin_31@apps.ipb.ac.id>
2020-05-17 22:48:06 +0700

    commit fragment_info.xml

--- app/src/main/java/com/example/elis/info.java
+++ app/src/main/java/com/example/elis/info.java
@@ -1,5 +1,7 @@
 package com.example.elis;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.protobuf.NullValue;
+
+import java.util.ArrayList;
 
 
 /**
@@ -55,10 +71,47 @@ public class info extends Fragment {
         }
     }
 
+    BarChart barChart;
+
+    @SuppressLint("ResourceAsColor")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+        barChart = view.findViewById(R.id.charthariini);
+        barChart.setDrawBarShadow(false);
+        barChart.setDrawValueAboveBar(true);
+        barChart.setMaxVisibleValueCount(4);
+        barChart.setPinchZoom(false);
+        barChart.setBackgroundColor(android.R.color.darker_gray);
+        barChart.setGridBackgroundColor(android.R.color.white);
+        barChart.getDescription().setEnabled(false);
+        barChart.setExtraOffsets(5, 10 ,5, 5);
+        barChart.setDragDecelerationFrictionCoef(0.95f);
+
+        ArrayList<BarEntry> yValues = new ArrayList<>();
+        yValues.add(new BarEntry(1,40f));
+        yValues.add(new BarEntry(2,30f));
+        yValues.add(new BarEntry(3,22f));
+        yValues.add(new BarEntry(4,53f));
+        yValues.add(new BarEntry(5,49f));
+        yValues.add(new BarEntry(6,37f));
+
+        BarDataSet dataSet = new BarDataSet(yValues,"asal");
+        dataSet.setBarShadowColor(android.R.color.background_dark);
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        BarData data = new BarData(dataSet);
+        data.setValueTextSize(10f);
+        data.setValueTextColor(android.R.color.white);
+        data.setBarWidth(0.7f);
+
+
+        barChart.setData(data);
+
+
+        return view;
+
+
     }
 }
